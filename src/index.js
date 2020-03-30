d70ed34d8c3c2acb5b6e02957a93420c30fde8d0
kaminion <anysweetimes@naver.com>
2020-03-30 19:23:33 +0900

    [2020-03-30] init redux

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementsByTagName("span");
+
+const handleAdd = () => {
+
+};
+
+const handleMinus = () => {
+
+};
+
+add.addEventListener("click", handleAdd);
+add.addEventListener("click", handleMinus);
