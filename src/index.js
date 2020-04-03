63c6ab8f09c9a613e55f3bc4e84af92ed025b480
kaminion <anysweetimes@naver.com>
2020-04-03 23:57:14 +0900

    리덕스 리팩토링

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
+import {createStore} from 'redux';
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
-const number = document.getElementsByTagName("span");
+const number = document.getElementsByTagName("span")[0];
+
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const handleAdd = () => {
-
+    countStore.dispatch({type : ADD});
 };
 
 const handleMinus = () => {
-
+    countStore.dispatch({type : MINUS});
 };
 
+const reducer = (state = 0, action)=>{
+    // 스위치 케이스문으로 바뀜
+    // if(action.type === "ADD"){
+    //     return ++state;
+    // }else if(action.type === "MINUS")
+    // {
+    //     return --state;
+    // }
+    // else{
+    //     return state;
+    // }
+    switch(action.type)
+    {
+        case ADD: return ++state;
+        case MINUS: return --state;
+        default:
+            return state;
+    }
+
+};
+const countStore = createStore(reducer);
+
+countStore.subscribe(() => {
+    number.innerText = countStore.getState();
+})
+
+
 add.addEventListener("click", handleAdd);
-add.addEventListener("click", handleMinus);
+minus.addEventListener("click", handleMinus);
+
